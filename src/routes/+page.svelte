2d4d2ebaa76df8ec92c18c04383b17044aa87d99
Coding4Hours <susybaka@mail.com>
2024-11-23 13:37:44 -0800

    aaaaaaaaaaaaaaaaaa

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    /** @type {(action: string, frame: string) => void} */
+    let chemicalAction;
+</script>
+
+<input autofocus spellcheck="false" autocomplete="off" id="search" data-frame="web" data-auto-https data-search-engine="https://www.google.com/search?q=%s" placeholder="Search or Enter a URL" is="chemical-input">
+<section id="controls" is="chemical-controls">
+    <button onclick={() => chemicalAction('back', 'web')}>Back</button>
+    <button onclick={() => chemicalAction('forward', 'web')}>Forward</button>
+    <button onclick={() => chemicalAction('reload', 'web')}>Reload</button>
+    <button onclick={() => chemicalAction('close', 'web')}>Close</button>
+</section>
+<iframe title="proxy stuff" id="web" data-controls="controls" is="chemical-iframe"></iframe>
